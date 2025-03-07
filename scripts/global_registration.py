572152c16bf6c0dbc0410e189f391aff112ef70d
Quang Huan Dong <huandq@gmail.com>
2025-03-07 11:55:13 +0700

    initial upload

---
+++ scripts/global_registration.py
@@ -0,0 +1,89 @@
+# ----------------------------------------------------------------------------
+# 20250307-QHD: Based on Open3D example and documentation
+# https://github.com/isl-org/Open3D/blob/main/examples/python/benchmark/benchmark_fgr.py
+# https://www.open3d.org/docs/latest/tutorial/pipelines/global_registration.html 
+# ----------------------------------------------------------------------------
+
+import copy
+import os
+import sys
+import numpy as np
+import open3d as o3d
+
+def execute_global_ransac_registration(source_down, target_down, source_fpfh,
+                                target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, True,
+        distance_threshold,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        3, [
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                0.9),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
+    return result
+
+def preprocess_point_cloud_source(pcd):
+
+    voxel_size = 0.001
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd, pcd_fpfh
+
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+def scale_point_cloud(point_cloud, scale_factor=1000.0):
+    """
+    Scales the given Open3D point cloud by the specified factor.
+    Default: Converts mm to meters by dividing by 1000.
+    """
+    point_cloud.points = o3d.utility.Vector3dVector(np.asarray(point_cloud.points) / scale_factor)
+    return point_cloud
+
+def draw_registration_result(source, target, transformation):
+
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    #source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([1, 0, 0])
+    source_temp.transform(transformation)
+
+    # TODO: Study why do we need this
+    flip_transform = [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]
+
+    source_temp.transform(flip_transform)
+    target_temp.transform(flip_transform)
+
+
+    o3d.visualization.draw_geometries([source_temp, target_temp])
+
+
